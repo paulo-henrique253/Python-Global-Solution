617371339e237e45bd295f1f58c35c0e2a9a400f
Paulo Henrique <paulo253hgg@gmail.com>
2026-06-06 16:09:37 -0300

    Refactor: comentarios

--- app/utils/data.py
+++ app/utils/data.py
@@ -5,25 +5,30 @@ sys.path.append(
     os.path.abspath("./app")
 )
 from models.empresa import Empresa
-from models.satelite import Satelite
+
+#from models.satelite import Satelite
+import models.satelite as sat
 empresas = list()
 
-def adicionar_empresa(nome: str, pais: str, num_sateliets: int,  documentacao) -> None:
-    empresa = Empresa(nome, pais, num_sateliets, documentacao)
+def adicionar_empresa(nome: str, pais: str) -> None:
+    empresa = Empresa(nome, pais)
     empresas.append(empresa)
 
-def adicionar_satelite(nome: str, nome_empresa: str, orbita: str, status: str) -> None:
-    satelite = Satelite(nome, nome_empresa, orbita, status)
+def adicionar_satelite(nome: str, nome_empresa: str, orbita: sat.TipoOrbita, status: sat.StatusOperacao) -> None:
+    satelite = sat.Satelite(nome, nome_empresa, orbita, status)
     for empresa in empresas:
         if empresa.nome == satelite.empresa:
             empresa.satelites.append(satelite)
+            return
+    print("ERRO!!! EMPRESA NÃO ENCONTRADA!!!")
+    
 
 
-adicionar_empresa("SpaceX", "States", 67, "")
+adicionar_empresa("SpaceX", "States")
 
-adicionar_empresa("Goon space", "Br", 69, "")
+adicionar_empresa("Goon space", "Br")
 
-adicionar_empresa("Zeni", "Brasil", 2, "")
+adicionar_empresa("Zeni", "Brasil")
 empresas[1].score = 30
 empresas[2].score = 67
 empresas[0].score = 40
