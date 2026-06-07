5d3c3b037f8798da4f048da9d8fd91ba3de84623
lixzaura <juliafb1903@gmail.com>
2026-06-07 10:46:31 -0300

    fix: texto do sobre

--- app/utils/funcs.py
+++ app/utils/funcs.py
@@ -353,10 +353,7 @@ def descricao_solucao() -> None:
     print("""
 ===== CONHEÇA A ORBITS =====
           
-          blablablablablablabla
-          blablablablablablabla
-          blablablablablablabla
-          blablablablablablabla
-          blablablablablablabla
+A Orbits é uma plataforma pública de transparência e governança da economia espacial que centraliza informações sobre o espaço. Utilizamos os dados das diversas empresas do ecossistema espacial para criar um ambiente de monitoramento de suas ações, tornando capaz que monitore satélites ativos e empresas cadastradas. Utilizamos um score para as empresas que pode varias caso suas ações sejam positivas ou negativas para o meio ambiente. Nosso objetivo é tornar o espaço um lugar seguro e com impactos positivos no futuro da humanidade, livre da exploração indevida.
+
           
           """)
